855b2c2a7b1a8b69fb0571bab65ff97d3592454f
Md Tasnim Farhan Fatin <86250039+fatin-farhan@users.noreply.github.com>
2025-02-24 20:42:20 -0800

    Update piece.py

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,35 +1,50 @@
-from .constants import RED, WHITE, SQUARE_SIZE, GREY
+from .constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
+import pygame
 
 class Piece:
-    PADDING = 10
+    #Padding for the piece position in each square
+    PADDING = 15
+    #Outline for the piece in each square
     OUTLINE = 2
-
-
+    # Each piece has position attribute (row,col), a color attribute (RED/WHITE), and a king mark
     def __init__(self, row, col, color):
+        # row position for the piece
         self.row = row
+        # column position for the piece
         self.col = col
+        # color position for the piece
         self.color = color
+        # If the piece is king or not
         self.king = False
-        
-        if self.color == RED:
-            self.direction = -1
-        else:
-            self.direction = 1
-
+        # Horizontal position will be stored
         self.x = 0
-        self.y = 0 
+        # Vertical Position will be stored
+        self.y = 0
+        # Calculate the position of each piece
+        self.calc_pos()
 
     def calc_pos(self):
+        #Piece will be placed in the center of a square
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
-        self.y = SQUARE_SIZE + self.row + SQUARE_SIZE // 2
-
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+    
     def make_king(self):
-        self.king = True 
-
+        #If the piece is king, king attribute will be set
+        self.king = True
+    
     def draw(self, win):
+        # radius of piece
         radius = SQUARE_SIZE//2 - self.PADDING
-        pygame.draw.circle(win, GREY,(self.x, self.y), radius + self.OUTLINE)
-        pygame.draw.circle(win, self.color,(self.x, self.y), radius)
+        # Creating an outline for a piece
+        pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
+        #Inserting piece in its position
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        # Inserting a crown logo for the king
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
 
-    def __repr__(self):
-        return str(self.color)    
+    def move(self, row, col):
+        #Updating the (x,y) position of a piece
+        self.row = row
+        self.col = col
+        self.calc_pos()
