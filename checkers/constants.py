cfee1da64caf36bcc11fb56b7fdabfe4f0d982e7
Md Tasnim Farhan Fatin <86250039+fatin-farhan@users.noreply.github.com>
2025-03-16 20:49:35 -0700

    Update constants.py

--- checkers/constants.py
+++ checkers/constants.py
@@ -1,9 +1,9 @@
-#List of Variables used
+#Variable
 import pygame
 
 WIDTH,HEIGHT = 560,560
 ROWS,COLS = 8,8
-SQUARE_SIZE=WIDTH//COLS #SQUARE SIZE=70
+SQUARE_SIZE=WIDTH//COLS
 
 #rgb
 RED=(255,0,0)
@@ -11,6 +11,6 @@ WHITE=(255,255,255)
 BLACK=(0,0,0)
 GREY=(128,128,128)
 BLUE=(0,0,255)
+GREEN = (0, 255, 0)
 
-# Crown image for king
 CROWN=pygame.transform.scale(pygame.image.load('assets/crown.png'),(44,25))
